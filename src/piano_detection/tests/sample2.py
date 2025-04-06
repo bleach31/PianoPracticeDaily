d7f2d0272ae10db0ccc519f64c1d080a9a6310c3
Motohiro Shibakawa <c201075@gmail.com>
2025-04-06 10:41:27 +0200

    add sample2

--- src/piano_detection/tests/sample2.py
+++ src/piano_detection/tests/sample2.py
@@ -6,4 +6,4 @@ context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by(subsystem='usb')
 for action, device in monitor:
-    print('{0}: {1}'.format(action, device.get('ID_MODEL')))
+    print('{0}: {1} :{2} :{3} :{4} :{5} :{6}'.format(action, device.get('ID_MODEL'), device.get("ID_USB_MODEL_ID") , device.get("PRODUCT"), device.sys_name, device.get("ID_VENDOR_ID"), device.get("ID_MODEL_ID"))) 
